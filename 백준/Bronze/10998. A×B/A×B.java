d31c40bd4a54d7effb39460e3818026653a7de82
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-07-26 22:00:11 +0900

    [Bronze V] Title: A×B, Time: 108 ms, Memory: 12888 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+        
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        
+        System.out.println(a*b);
+        
+    }
+}
