997d29bc74ce827a186ea038a415b650873219e9
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-07-28 17:13:52 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+			
+	public static void main(String[]agrs){
+		
+		String str = "";
+		
+		Scanner sc = new Scanner(System.in);
+		str = sc.nextLine();
+		
+		System.out.println(str + "??!");
+	   
+	}
+
+}
