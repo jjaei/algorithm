eecc35d8df370de0acd136327af36aeaa3f24e71
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-07-28 15:11:41 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+			
+		    public static void main(String[]agrs){
+		        
+		        int a, b;
+		        
+		        Scanner scanner = new Scanner(System.in);
+		        
+		        a = scanner.nextInt();
+		        b = scanner.nextInt();
+		        
+		        System.out.println(a+b);		
+		        System.out.println(a-b);		
+		        System.out.println(a*b);	
+		        System.out.println(a/b);	
+		        System.out.println(a%b);		
+	}
+
+}
