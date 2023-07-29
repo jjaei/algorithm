7aa5955c499672bb93faed69f0b239a58e03b700
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-07-29 13:06:06 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 112 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+			
+	int y =0;
+	Scanner sc = new Scanner(System.in);
+	y = sc.nextInt();
+	
+	System.out.println(y-543);
+			
+	}
+}
