7ac33ae9f34171b09d99dc544f82b5f2113508b5
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-08-02 00:48:38 +0900

    [Bronze III] Title: 알람 시계, Time: 108 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+			if(m < 45) {
+				if(h==0) {
+					h=23;
+				} else {
+					h--;
+				}
+				m = 60-45+m;
+			} else {
+				m = m-45;
+
+			}
+			
+			System.out.println(h + " " + m);
+		
+	}
+
+}
