762e5fc72e1b64a1959890543cbfac33e130847a
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-08-06 14:26:09 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 116 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int ii = sc.nextInt();
+		for(int i=0; i<ii/4; i++) {
+			System.out.print("long ");
+		}
+		System.out.print("int");
+	}
+}
