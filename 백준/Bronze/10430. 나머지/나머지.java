4a67f0184dfa4ef653e19857e3f55176e501fff8
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-08-15 15:40:58 +0900

    [Bronze V] Title: 나머지, Time: 80 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,15 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        System.out.println((A+B)%C);
-        System.out.println(((A%C)+(B%C))%C);
-        System.out.println((A*B)%C);
-        System.out.println(((A%C)*(B%C))%C);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.valueOf(st.nextToken());
+        int B = Integer.valueOf(st.nextToken());
+        int C = Integer.valueOf(st.nextToken());
+
+        bw.write((A + B) % C + "\n");
+        bw.write(((A % C) + (B % C)) % C + "\n");
+        bw.write((A * B) % C + "\n");
+        bw.write(((A % C) * (B % C)) % C+ "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
