f7399ad2b43645a7ae0b616ea0be176e10450ead
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-08-20 20:06:51 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 80 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,14 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int ii = sc.nextInt();
-		for(int i=0; i<ii/4; i++) {
-			System.out.print("long ");
-		}
-		System.out.print("int");
-	}
+        int n = Integer.valueOf(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 4 == 0) {
+                bw.write("long ");
+            }
+            if(i==n){
+                bw.write("int");
+            }
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
