ae41f930a416afbbb6f4b6aabbca7f86aa77c5a3
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-08-22 15:56:25 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 80 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,14 +3,17 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.valueOf(br.readLine());
         String star = "";
 
         for (int i = 0; i < n; i++) {
             star += "*";
-            System.out.println(star);
+            bw.write(star + "\n");
         }
         br.close();
+        bw.flush();
+        bw.close();
     }
 }
