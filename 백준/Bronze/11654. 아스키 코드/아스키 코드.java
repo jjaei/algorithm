a79f5a1e9bc7b766714556e8d64faede8b0402ae
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-08-22 15:49:27 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 12784 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int ch = sc.next().charAt(0);
+		System.out.print(ch);
+		
+	}
+
+}
