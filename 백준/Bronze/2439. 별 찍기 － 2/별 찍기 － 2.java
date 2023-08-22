8916936419439a1da66da012307ccbe6bec1313b
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-08-22 16:10:08 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 88 ms, Memory: 11804 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -3,17 +3,21 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.valueOf(br.readLine());
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n-i; j++) {
-                System.out.print(" ");
+                bw.write(" ");
             }
             for (int k = 1; k <= i; k++) {
-                System.out.print("*");
+                bw.write("*");
             }
-            System.out.println();
+            bw.write("\n");
         }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
