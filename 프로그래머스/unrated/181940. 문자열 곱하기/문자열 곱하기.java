9791dc3bffe7bc00dadb1495fc9322ca8735003b
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-10-01 14:32:41 +0900

    [unrated] Title: 문자열 곱하기, Time: 1.88 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public String solution(String my_string, int k) {
+
+        String answer = "";
+
+        for (int i = 0; i < k; i++) {
+            answer += my_string;
+        }
+        return answer;
+
+    }
+}
