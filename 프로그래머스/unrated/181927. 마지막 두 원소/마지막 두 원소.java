707c4e680df5e061c27541e92d10e673decb554f
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-10-06 19:30:35 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length + 1];
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = num_list[i];
+            if (i == num_list.length - 2) {
+                answer[i] = num_list[i];
+                if (num_list[i] < num_list[i + 1]) {
+                    answer[i + 2] = num_list[i + 1] - num_list[i];
+                } else {
+                    answer[i + 2] = num_list[i + 1] * 2;
+                }
+            }
+        }
+        return answer;
+    }
+}
