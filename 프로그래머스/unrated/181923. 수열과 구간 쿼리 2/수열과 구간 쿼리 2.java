5e78d5c9b29eef819abc5d27da4d87b97b096eef
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-10-08 15:36:57 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 3.99 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        Arrays.fill(answer, Integer.MAX_VALUE);
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
+                if (arr[j] > queries[i][2]) {
+                    if (answer[i] > arr[j]) {
+                        answer[i] = Math.min(arr[j], answer[i]);
+                    }
+                }
+            }
+            if (answer[i] == Integer.MAX_VALUE) {
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
