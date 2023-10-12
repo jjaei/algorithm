0d5c1f8c22c7daa1e14b474c61fe88749f2581e0
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-10-12 16:29:55 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 0.04 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        int num = n;
+        for (int i = 1; i < n; i++) {
+
+            if (num == 1) {
+                list.add(num);
+                break;
+            }
+
+            if (num % 2 == 0) {
+                list.add(num);
+                num = num / 2;
+            } else if (num % 2 != 0) {
+                list.add(num);
+                num = (3 * num) + 1;
+            }
+        }
+        int[] answer = new int[list.size()];
+        for (int j = 0; j < answer.length; j++) {
+            answer[j] = list.get(j);
+        }
+        return answer;
+    }
+}
