33a23891bd1dd9fa6fb18c303b9d3ced67120c27
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-10-15 16:59:52 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.charAt(i) >= 'A' && my_string.charAt(i) <= 'Z') {
+                answer[my_string.charAt(i) - 65]++;
+            } else if (my_string.charAt(i) >= 'a' && my_string.charAt(i) <= 'z') {
+                answer[my_string.charAt(i) - 71]++;
+            }
+        }
+        return answer;
+    }
+}
