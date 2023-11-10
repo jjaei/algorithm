55a1e27162d09b8a5606e77b301a536397761771
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-11-11 00:02:40 +0900

    [level unrated] Title: l로 만들기, Time: 5.36 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder(myString);
+        int a = 'l';
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) < 'l') {
+                sb.setCharAt(i, 'l');
+            }
+        }
+        return sb.toString();
+    }
+}
