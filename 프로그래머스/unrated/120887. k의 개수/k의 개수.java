6f4994eeaebd349c3b555e5c1fec9f437636fc8d
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-11-18 21:38:45 +0900

    [level unrated] Title: k의 개수, Time: 1.86 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        for (int a = i; a <= j; a++) {
+            int num = a;
+            while (num > 0) {
+                if (num % 10 == k) {
+                    answer++;
+                    num /= 10;
+                } else {
+                    num /= 10;
+                }
+            }
+        }
+        return answer;
+    }
+}
