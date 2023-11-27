7523e8af8b89aee8c589deb35615c6db676a6d59
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-11-27 19:21:12 +0900

    [level unrated] Title: 7의 개수, Time: 0.03 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
@@ -1,16 +1,14 @@
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        for (int i = 0; i < array.length;) {
-            while(array[i] > 0) {
-                if (array[i] % 10 == 7) {
+
+        for (int i : array) {
+            while (i > 0) {
+                if (i % 10 == 7) {
                     answer++;
-                    array[i] /= 10;
-                } else {
-                    array[i] /= 10;
                 }
+                i /= 10;
             }
-            i++;
         }
         return answer;
     }
