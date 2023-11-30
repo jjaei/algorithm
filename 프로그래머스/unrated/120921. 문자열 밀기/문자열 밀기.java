147cf31dc7bd38a7d57419eeccd5bb53614af4cb
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-11-30 21:37:19 +0900

    [level unrated] Title: 문자열 밀기, Time: 1.70 ms, Memory: 71.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        String C = A;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < A.length(); i++) {
+            if(C.equals(B)) return answer;
+
+            String D = C.substring(C.length() - 1);
+            C = D + C.substring(0, C.length() - 1);
+            answer++;
+        }
+        return -1;
+    }
+}
