5b67448fbd2cf017bbcf01a7e8a1d22a1de92d50
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-12-02 14:01:46 +0900

    [level unrated] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        if (common[1] - common[0] == common[2] - common[1]) {
+            // 등차수열인 경우
+            answer = common[common.length - 1] + common[1] - common[0];
+        } else {
+            // 등비수열인 경우
+            answer = common[common.length - 1] * (common[1] / common[0]);
+        }
+        return answer;
+    }
+}
