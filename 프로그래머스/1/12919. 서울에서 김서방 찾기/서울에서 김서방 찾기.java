5866a7c66536319a1b85e9b02c4aeabd9ffd1ba4
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-12-17 19:54:30 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.05 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int index = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                index = i;
+                break;
+            }
+        }
+        return "김서방은 " + index + "에 있다";
+    }
+}
