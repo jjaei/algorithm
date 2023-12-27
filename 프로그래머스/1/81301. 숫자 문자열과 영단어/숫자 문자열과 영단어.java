dcf5c31c43726192f075e8167e0ac8b900048268
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2023-12-27 20:33:31 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String s) {
+        String answer = s.replace("one", "1").
+                replace("two", "2").
+                replace("three", "3").
+                replace("four", "4").
+                replace("five", "5").
+                replace("six", "6").
+                replace("seven", "7").
+                replace("eight", "8").
+                replace("nine", "9").
+                replace("zero", "0");
+        return Integer.valueOf(answer);
+    }
+}
