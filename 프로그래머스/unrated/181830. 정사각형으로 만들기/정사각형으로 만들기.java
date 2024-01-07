7c2812d68d6e1959db27cb4edf13765720f946cc
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-01-07 14:53:07 +0900

    [level unrated] Title: 정사각형으로 만들기, Time: 0.43 ms, Memory: 95.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] solution(int[][] arr) {
+        int[][] answer = new int[arr[0].length > arr.length ? arr[0].length : arr.length][arr[0].length > arr.length ? arr[0].length : arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                answer[i][j] = arr[i][j];
+            }
+        }
+        return answer;
+    }
+}
