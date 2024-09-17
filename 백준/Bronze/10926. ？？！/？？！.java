f342d6b3a97258751319423eaab0e0d6be3a42c8
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-17 23:06:40 +0900

    [Bronze V] Title: ??!, Time: 164 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,16 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
-			
-	public static void main(String[]agrs){
-		
-		String str = "";
-		
-		Scanner sc = new Scanner(System.in);
-		str = sc.nextLine();
-		
-		System.out.println(str + "??!");
-	   
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(id + "??!");
+    }
 }
