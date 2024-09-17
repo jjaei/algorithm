d902cd2bc393d8fba83fdaef18d1e91193475c25
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-17 23:03:42 +0900

    [Bronze V] Title: 사칙연산, Time: 168 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-			
-		    public static void main(String[]agrs){
-		        
-		        int a, b;
-		        
-		        Scanner scanner = new Scanner(System.in);
-		        
-		        a = scanner.nextInt();
-		        b = scanner.nextInt();
-		        
-		        System.out.println(a+b);		
-		        System.out.println(a-b);		
-		        System.out.println(a*b);	
-		        System.out.println(a/b);	
-		        System.out.println(a%b);		
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
 }
