206dadc1d1997060570492aa49eb610427f29406
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-17 23:20:09 +0900

    [Bronze III] Title: 곱셈, Time: 168 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println(a * (b % 10));
-        System.out.println(a * (b % 100 / 10));
-        System.out.println(a * (b / 100));
-        System.out.println(a * b);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = 0;
+        int num4 = num2;
+        while (num2 > 0) {
+            num3 = num2 % 10;
+            System.out.println(num1 * num3);
+            num2 = num2 / 10;
+        }
+        System.out.println(num1 * num4);
     }
 }
