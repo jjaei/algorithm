3b4d34c8ada49f589586561cb6a77a3e3995c9c1
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-18 20:29:21 +0900

    [Bronze V] Title: 시험 성적, Time: 168 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-
-	int score = 0;
-	char grade =' ';
-	
-	Scanner sc = new Scanner(System.in);
-	score = sc.nextInt();
-	
-	if(score>=90) {
-		grade = 'A';
-	} else if(score >= 80) {
-		grade = 'B';
-	} else if(score >= 70) {
-		grade ='C';
-	} else if(score >= 60) {
-		grade = 'D';
-	} else {
-		grade ='F';
-	}
-		System.out.println(grade);
-  }		
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
 }
