beda9d83f9dd333dd4c5bf58e3f5911706b0b01d
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-18 20:33:46 +0900

    [Bronze V] Title: 윤년, Time: 168 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,22 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-
-	int year = 0;
-	Scanner sc = new Scanner(System.in);
-	year = sc.nextInt();
-	
-	if(year%4==0){
-		if(!(year%100==0)) {
-			System.out.println(1);
-		} else if(year%400==0) {
-			System.out.println(1);
-		} else {
-			System.out.println(0);
-		}
-	}else {
-		System.out.println(0);
-	}
-  }		
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
 }
