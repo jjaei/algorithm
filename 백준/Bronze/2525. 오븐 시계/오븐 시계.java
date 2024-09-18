6ef8bf745eec716bf63fc1ee68524015b9871e27
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-18 20:50:08 +0900

    [Bronze III] Title: 오븐 시계, Time: 200 ms, Memory: 18520 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();  // 현재 시각 - 시
-        int b = sc.nextInt();  // 현재 시각 - 분
-        int c = sc.nextInt();  // 요리하는데 필요한 시간 - 분
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int time = sc.nextInt();
+        int sum = (h * 60) + m + time;
 
-        int sum = (a * 60) + b + c;
-        int hour = sum / 60 % 24;
+        int hour = sum / 60;
         int minute = sum % 60;
+
+        if (hour > 23) {
+            hour -= 24;
+        }
 
         System.out.println(hour + " " + minute);
     }
