b85289d00a0860380f25304f7584d19c7b0afbd5
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-20 18:28:55 +0900

    [Bronze V] Title: 구구단, Time: 188 ms, Memory: 18528 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,16 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Scanner sc = new Scanner(System.in);
-		int gu = sc.nextInt();
-		
-		for(int i=1; i<=9; i++) {
-			System.out.printf("%d * %d = %d%n", gu, i, gu*i);
-		}
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+    }
 }
