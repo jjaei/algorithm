d74b87bfbb0589b2107eaab72a0e65827cd5f780
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-20 18:31:34 +0900

    [Bronze V] Title: 합, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int n = sc.nextInt();
         int sum = 0;
-
-        for (int i = 0; i <= num; i++) {
+        for (int i = 0; i <= n; i++) {
             sum += i;
         }
-
         System.out.println(sum);
     }
 }
