3db285a1a0ade426bdd5c0e85108a64bcda8614d
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-20 18:51:38 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 792 ms, Memory: 270348 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,26 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			sb.append(Integer.parseInt(st.nextToken())
-					+ Integer.parseInt(st.nextToken())).append('\n');
-		}
-		
-		br.close();
-		
-		System.out.println(sb);
-	}
+        int num = Integer.valueOf(st.nextToken());
+
+        for (int i = 0; i < num; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int a = Integer.valueOf(st2.nextToken());
+            int b = Integer.valueOf(st2.nextToken());
+            bw.write(String.valueOf(a + b ) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
 }
-
