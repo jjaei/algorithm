588c476d01537eba6beec428983efc6e8b6898e6
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-24 22:59:53 +0900

    [Bronze III] Title: 최소, 최대, Time: 2132 ms, Memory: 322244 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -2,22 +2,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	public static void main (String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int max = 1000000;
-		int min = -1000000;
-		int n = sc.nextInt();
-		int[] arry = new int[n];
-		
-		for(int i=0; i<n; i++) {
-			arry[i] = sc.nextInt();
-			if(arry[i]<max)
-				max = arry[i];
-			if(arry[i]>min)
-				min = arry[i];
-		} 
-		
-		System.out.println(max + " " + min);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[arr.length - 1]);
+    }
 }
