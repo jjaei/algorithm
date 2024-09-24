049d7772f4cea456c2af8b1b249043be67b123e1
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-24 22:46:05 +0900

    [Bronze V] Title: A+B - 4, Time: 196 ms, Memory: 17948 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,18 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main (String[] args)   {
-	
-		Scanner sc = new Scanner(System.in);
-		int a =0, b=0;
-		
-		while(sc.hasNextInt()) {
-			a=sc.nextInt();
-			b=sc.nextInt();
-
-			System.out.println(a+b);
-		}	
-		
-	}
-		
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
 }
