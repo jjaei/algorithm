7829734f41afe48254ea07982dc6589b297a0b6c
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-25 20:28:24 +0900

    [Bronze V] Title: A/B, Time: 168 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextInt();
+        double num2 = sc.nextInt();
+        System.out.println(num1 / num2);
+    }
+}
