5ef354bb7d54a714b3cfe165f5a641cf96288f06
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-25 22:59:40 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 172 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,44 +1,17 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int[] student = new int[30];
-        int[] homework = new int[28];
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean[] check = new boolean[30];
         for (int i = 0; i < 28; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            homework[i] = Integer.valueOf(st.nextToken());
+            int num = sc.nextInt();
+            check[num - 1] = true;
         }
-
-        for (int j = 0; j < student.length; j++) {
-            student[j] = j + 1;
-        }
-
-        for (int num : homework) {
-            for (int k = 0; k < student.length; k++) {
-                if (num == student[k]) {
-                    student[k] = 0;
-                    break;
-                }
+        for (int j = 0; j < check.length; j++) {
+            if (!check[j]) {
+                System.out.println(j + 1);
             }
         }
-
-        for (int l = 0; l < student.length; l++) {
-            if (student[l] != 0) {
-                bw.write(student[l] + "\n");
-            }
-        }
-
-        br.close();
-        bw.flush();
-        bw.close();
-
     }
 }
