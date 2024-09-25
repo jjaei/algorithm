20fef04f9c53f732b8aa1b7bf5b786437807cb44
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-25 20:27:22 +0900

    [Bronze II] Title: 공 바꾸기, Time: 196 ms, Memory: 18480 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,36 +1,28 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.valueOf(st.nextToken()); // 바구니의 수
-        int m = Integer.valueOf(st.nextToken()); // 공을 바꿀 횟수
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // 바구니 갯수
+        int m = sc.nextInt();  // 공을 바꿀 횟수
         int[] basket = new int[n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < basket.length; i++) {
             basket[i] = i + 1;
         }
 
         for (int j = 0; j < m; j++) {
-            StringTokenizer input = new StringTokenizer(br.readLine());
-            int start = Integer.valueOf(input.nextToken());
-            int end = Integer.valueOf(input.nextToken());
-            int tmp = basket[start - 1];
-            basket[start - 1] = basket[end - 1];
-            basket[end - 1] = tmp;
-            }
-        for (int k = 0; k < n; k++) {
-            bw.write(basket[k] + " ");
+            // a와 b의 공을 교환한다.
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int tmp = basket[a - 1];
+            basket[a - 1] = basket[b - 1];
+            basket[b - 1] = tmp;
         }
 
-        br.close();
-        bw.flush(); // 버퍼에 남아있는 데이터 모두 출력
-        bw.close();
+        for (int k = 0; k < basket.length; k++) {
+            System.out.print(basket[k] + " ");
+        }
     }
 }
