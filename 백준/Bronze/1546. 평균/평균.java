b4cb5630ebe1df533dd16b9f25594d8f20fa24fa
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-25 23:13:03 +0900

    [Bronze I] Title: 평균, Time: 216 ms, Memory: 19140 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,33 +1,22 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.valueOf(br.readLine());
-        double[] score = new double[n];
-        double avg = 0.0;
-        double max = 0;
-        double sum = 0;
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < score.length; i++) {
-            score[i] = Double.valueOf(st.nextToken());
-            if (score[i] > max) {
-                max = score[i];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        double max = Double.MIN_VALUE;
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) {
+            int score = sc.nextInt();
+            if (max < score) {
+                max = score;
             }
+            arr[i] = score;
         }
-
-        for (int i = 0; i < score.length; i++) {
-            sum += score[i] / max * 100;
-            avg = sum / score.length;
+        double sum = 0;
+        for (int j = 0; j < arr.length; j++) {
+            sum += arr[j] / max * 100;
         }
-
-        System.out.println(avg);
-        br.close();
+        System.out.println(sum / num);
     }
 }
