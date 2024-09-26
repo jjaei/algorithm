610606f477d65c04593924ad5795f31f82758987
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-26 20:18:15 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 204 ms, Memory: 18508 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,42 +1,26 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.valueOf(st.nextToken());  // 바구니의 수
-        int m = Integer.valueOf(st.nextToken());  // m번 바구니의 순서부터 역순으로
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // 바구니의 갯수
+        int m = sc.nextInt();  // 바구니의 순서를 역순으로 만들 횟수
         int[] basket = new int[n];
-
         for (int i = 0; i < basket.length; i++) {
             basket[i] = i + 1;
         }
-
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine());
-            int start = Integer.valueOf(st2.nextToken());
-            int end = Integer.valueOf(st2.nextToken());
-
-            for (int j = 0; start+j < end-j; j++) {
-                int tmp = basket[start + j - 1];
-                basket[start + j - 1] = basket[end - j - 1];
-                basket[end - j - 1] = tmp;
+        for (int j = 0; j < m; j++) {
+            // start부터 end를 역순으로 만든다.
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            for (int k = 0; start + k < end - k; k++) {
+                int tmp = basket[start - 1 + k];
+                basket[start - 1 + k] = basket[end - 1 - k];
+                basket[end - 1 - k] = tmp;
             }
         }
-
-        for (int i = 0; i < basket.length; i++) {
-            bw.write(basket[i] + " ");
+        for (int l = 0; l < basket.length; l++) {
+            System.out.print(basket[l] + " ");
         }
-        br.close();
-        bw.flush();
-        bw.close();
     }
 }
