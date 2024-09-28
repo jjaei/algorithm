fc4dd7a5b2a0e07deaeeaa0a3ed79951fdacb03a
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-29 00:12:31 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 188 ms, Memory: 18276 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,27 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int[] alphabet = new int[26];
+        for (int i = 0; i < alphabet.length; i++) {
+            alphabet[i] = -1;
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int[] ap = new int[26];
-		
-		for(int i=0; i < ap.length; i++) {
-			ap[i] = -1;
-		}
-		
-		String s = sc.next();
-		for(int i=0; i<s.length(); i++) {
-			if(ap[s.charAt(i)-97] == -1) 
-				ap[s.charAt(i)-97] = i;
-
-		}
-		
-		for(int i=0; i<ap.length; i++) {
-			System.out.print(ap[i] + " ");
-		}
-		
-	}
-
+        for (int j = 0; j < s.length(); j++) {
+            if (alphabet[s.charAt(j) - 97] == -1) {
+                alphabet[s.charAt(j) - 97] = j;
+            }
+        }
+        for (int k = 0; k < alphabet.length; k++) {
+            System.out.print(alphabet[k] + " ");
+        }
+    }
 }
