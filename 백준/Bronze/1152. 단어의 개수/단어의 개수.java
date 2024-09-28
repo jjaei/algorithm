3a418f84e757e55feb6f4a3fc8ad16de08b39fc2
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-29 00:34:58 +0900

    [Bronze II] Title: 단어의 개수, Time: 492 ms, Memory: 62804 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,25 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main (String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine().trim();
-		int sum = 0;
-		int sp = 0;
-		for(int i=0; i<input.length(); i++) {
-			char ch = input.charAt(i);
-			if(ch == ' ') {
-				sp++;
-			} else {
-				sum++;
-			}
-		}
-		
-		if(sum != 0) {
-			System.out.println(sp+1);
-		} else {
-			System.out.println(0);
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.nextLine().trim().split(" ");
+        int answer = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (!s[i].isEmpty()) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
 }
