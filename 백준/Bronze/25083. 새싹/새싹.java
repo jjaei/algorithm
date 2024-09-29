bd5321f6559ede572410fe798ebae62d76a0f10b
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-29 09:49:28 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13912 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("         ,r'\"7");
         System.out.println("r`-_   ,'  ,/");
         System.out.println(" \\. \". L_r'");
