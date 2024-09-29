1fc7209134496b34436066eb1cfb90924510ad23
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-09-29 09:53:06 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 180 ms, Memory: 18312 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -4,12 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] chess = {1, 1, 2, 2, 2, 8};
-
         for (int i = 0; i < chess.length; i++) {
-            int piece = sc.nextInt();
-            chess[i] = chess[i] - piece;
-            System.out.print(chess[i]+" ");
+            int num = sc.nextInt();
+            System.out.print((chess[i] - num) + " ");
         }
-        sc.close();
     }
 }
