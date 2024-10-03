507b41d74d654e535ab4a6f0fc9e360948d882f4
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-10-03 22:26:08 +0900

    [Bronze II] Title: 다이얼, Time: 168 ms, Memory: 17556 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,34 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main (String[] args) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int time = 0;
 
-		Scanner sc = new Scanner(System.in);
-		String input = sc.next();
-		int time = 0;
-		
-		for(int i=0; i<input.length();i++) {
-			if(input.charAt(i)-0<=67) { // ABC
-				time += 3;
-			} else if(input.charAt(i)-0<=70) {  // DEF
-				time += 4;
-			} else if(input.charAt(i)-0<=73) {  // GHI
-				time += 5;
-			} else if(input.charAt(i)-0<=76) {  // JKL
-				time += 6;
-			} else if(input.charAt(i)-0<=79) {  // MNO
-				time += 7;
-			} else if(input.charAt(i)-0<=83) {  // PQRS
-				time += 8;
-			} else if(input.charAt(i)-0<=86) {  // TUV
-				time += 9;
-			} else if(input.charAt(i)-0<=90) {  // WXYZ
-				time += 10;
-			} else {
-				time += 11;
-			}
-	
-		}
-		System.out.println(time);
-	}	
+        for (int i = 0; i < str.length(); i++) {
+            switch(str.charAt(i)) {
+                case 'A': case 'B': case 'C':
+                    time += 3;
+                    break;
+                case 'D': case 'E': case 'F':
+                    time += 4;
+                    break;
+                case 'G': case 'H': case 'I':
+                    time += 5;
+                    break;
+                case 'J': case 'K': case 'L':
+                    time += 6;
+                    break;
+                case 'M': case 'N': case 'O':
+                    time += 7;
+                    break;
+                case 'P': case 'Q': case 'R': case 'S':
+                    time += 8;
+                    break;
+                case 'T': case 'U': case 'V':
+                    time += 9;
+                    break;
+                case 'W': case 'X': case 'Y': case 'Z':
+                    time += 10;
+                    break;
+            }
+        }
+        System.out.println(time);
+    }
 }
