112944a87cf3671a3d2bf71e3511285f83367be3
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-10-03 22:10:29 +0900

    [Bronze II] Title: 상수, Time: 164 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main (String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		String s1 = sc.next();
-		String s2 = sc.next();
-		
-		int num1 = Integer.parseInt(new StringBuffer(s1).reverse().toString());
-		int num2 = Integer.parseInt(new StringBuffer(s2).reverse().toString());
-		
-		System.out.println(num1 > num2 ? num1 : num2);
-
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String num1 = sc.next();
+        String num2 = sc.next();
+        StringBuilder sb1 = new StringBuilder(num1).reverse();
+        StringBuilder sb2 = new StringBuilder(num2).reverse();
+        for (int i = 0; i < num1.length(); i++) {
+            if (sb1.charAt(i) > sb2.charAt(i)) {
+                System.out.print(Integer.valueOf(sb1.toString()));
+                break;
+            } else if (sb1.charAt(i) < sb2.charAt(i)) {
+                System.out.print(Integer.valueOf(sb2.toString()));
+                break;
+            }
+        }
+    }
 }
