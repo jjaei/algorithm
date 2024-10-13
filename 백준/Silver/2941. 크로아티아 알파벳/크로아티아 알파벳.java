3016e5551c7ed83192d3246482c9f861b5243696
Jaei Kwon <120344687+jjaei@users.noreply.github.com>
2024-10-13 23:16:13 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 160 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -3,59 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        int check = 0;
-
+        String str = sc.next();
+        int answer = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'c'  && i + 1 < str.length()) {
-                if (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
-                    check++;
+            if (str.charAt(i) == 'c') {
+                if (i + 1 < str.length() && (str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-')) {
                     i++;
-                } else {
-                    check++;
                 }
-            } else if (str.charAt(i) == 'd' && i + 1 < str.length()) {
-                if (i + 2 < str.length() && str.charAt(i + 1) == 'z' && str.charAt(i + 2) == '=') {
-                    check++;
-                    i += 2;
-                } else if (str.charAt(i + 1) == '-') {
-                    check++;
+            } else if(str.charAt(i) == 'd') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == '-') {
                     i++;
-                } else {
-                    check++;
+                } else if (i + 1 < str.length() && str.charAt(i + 1) == 'z') {
+                    if (i + 2 < str.length() && str.charAt(i + 2) == '=') {
+                        i += 2;
+                    }
                 }
-            } else if (str.charAt(i) == 's' && i + 1 < str.length()) {
-                if (str.charAt(i + 1) == '=') {
-                    check++;
+            } else if (str.charAt(i) == 'l') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == 'j') {
                     i++;
-                } else {
-                    check++;
                 }
-            } else if (str.charAt(i) == 'z' && i + 1 < str.length()) {
-                if (str.charAt(i + 1) == '=') {
-                    check++;
+            } else if (str.charAt(i) == 'n') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == 'j') {
                     i++;
-                } else {
-                    check++;
                 }
-            } else if (str.charAt(i) == 'l' && i + 1 < str.length()) {
-                if (str.charAt(i + 1) == 'j') {
-                    check++;
+            } else if (str.charAt(i) == 's') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == '=') {
                     i++;
-                } else {
-                    check++;
                 }
-            } else if (str.charAt(i) == 'n' && i + 1 < str.length()) {
-                if (str.charAt(i + 1) == 'j') {
-                    check++;
+            } else if (str.charAt(i) == 'z') {
+                if (i + 1 < str.length() && str.charAt(i + 1) == '=') {
                     i++;
-                } else {
-                    check++;
                 }
-            } else {
-                check++;
             }
+            answer++;
         }
-        System.out.print(check);
+        System.out.println(answer);
     }
 }
